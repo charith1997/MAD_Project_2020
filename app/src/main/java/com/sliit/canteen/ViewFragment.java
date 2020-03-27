c1d3f56b8497967b995904d6bc42203c71d6bb5a
VImadhuri <49853682+VImadhuri@users.noreply.github.com>
2020-03-27 06:31:31 +0530

    Canteen owner
    UI For canteen owner

---
+++ app/src/main/java/com/sliit/canteen/ViewFragment.java
@@ -0,0 +1,20 @@
+package com.sliit.canteen;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class ViewFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_view, container, false);
+    }
+
+
+}
